3d024ad5010a2fd3b96fd1605c7dc80d42cdfeae
Joram Barrez <joram.barrez@gmail.com>
2016-04-28 21:44:05 +0200

    identation

--- pluggable-persistence-example/src/main/java/org/activiti/Main.java
+++ pluggable-persistence-example/src/main/java/org/activiti/Main.java
@@ -22,17 +22,22 @@ public class Main {
     TaskService taskService = processEngine.getTaskService();
     HistoryService historyService = processEngine.getHistoryService();
     
-    Deployment deployment = repositoryService.createDeployment().addClasspathResource("oneTaskProcess.bpmn20.xml").deploy();
+    Deployment deployment = repositoryService.createDeployment()
+    		.addClasspathResource("oneTaskProcess.bpmn20.xml").deploy();
     System.out.println("Process deployed! Deployment id is " + deployment.getId());
     
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("oneTaskProcess");
-    List<Task> tasks = taskService.createTaskQuery().processInstanceId(processInstance.getId()).list();
+    List<Task> tasks = taskService.createTaskQuery()
+    		.processInstanceId(processInstance.getId()).list();
     System.out.println("Got " + tasks.size() + " tasks!");
     
     taskService.complete(tasks.get(0).getId());
-    System.out.println("Number of process instances = " + historyService.createHistoricProcessInstanceQuery().count());
-    System.out.println("Number of active process instances = " + historyService.createHistoricProcessInstanceQuery().finished().count());
-    System.out.println("Number of finished process instances = " + historyService.createHistoricProcessInstanceQuery().unfinished().count());
+    System.out.println("Number of process instances = " 
+    		+ historyService.createHistoricProcessInstanceQuery().count());
+    System.out.println("Number of active process instances = " 
+    		+ historyService.createHistoricProcessInstanceQuery().finished().count());
+    System.out.println("Number of finished process instances = " 
+    		+ historyService.createHistoricProcessInstanceQuery().unfinished().count());
     
   }
 
